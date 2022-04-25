162ed9d7f9ed2016641f15a91aac0505369fc218
popaClaudiu <54347198+popaClaudiu@users.noreply.github.com>
2022-04-25 09:50:50 +0300

    Add run instant uptime run

--- licenta/resources/views/livewire/sites-table.blade.php
+++ licenta/resources/views/livewire/sites-table.blade.php
@@ -19,6 +19,9 @@
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Broken Links
                     </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Actions
+                    </th>
                 </tr>
                 </thead>
                 <tbody>
@@ -71,6 +74,9 @@
                                         'error' => ['message' => 'Found']
                                     ])
                                 </a>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                <button class="text-green-500 hover:text-green-600" wire:click="dispatchUptimeEvent({{$site->id}})"><i class="fa-solid fa-play"></i></button>
                             </td>
                         </tr>
                     @empty
