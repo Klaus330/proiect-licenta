2e3e3411427f1a0c1a74b06394476be4aa55b975
popaClaudiu <54347198+popaClaudiu@users.noreply.github.com>
2022-04-29 20:34:00 +0300

    Feat: Add landing page

---
+++ licenta/resources/views/homepage/sections/faq.blade.php
@@ -0,0 +1,58 @@
+<div class="bg-indigo-900 px-4 py-10 flex flex-col items-center justify-center" id="faq">
+    <div class="flex flex-col items-start justify-start md:items-center md:justify-center max-w-lg m-auto">
+        <h2 class="text-white text-3xl font-bold">Frequently asked questions</h2>
+        <p class="text-sm text-gray-300 text-left md:text-center py-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum enim libero quasi possimus accusantium adipisci non.
+        </p>
+    </div>
+    <div class="pt-3 pb-1 md:py-3 grid grid-cols-1 md:grid-cols-2 w-full md:gap-3 max-w-4xl">
+        <div class="flex flex-col justify-start p-3 gap-3">
+            <div class="py-2">
+                <h3 class="text-white text-lg font-bold">What timezone is your server configured in?</h3>
+                <p class="text-sm text-gray-300">
+                    Our servers are primarly located in the EU.
+                </p>
+            </div>
+            <div class="py-2">
+                <h3 class="text-white text-lg font-bold">What is a "cron job expression"?</h3>
+                <p class="text-sm text-gray-300">
+                    A cron job expression is a way for us to specify the frequency in minutes 
+                    for your schedulers. For example, if you want your scheduler to run every minute
+                    you can use the expression "* * * * *". For more information we recommand reading
+                    this <a href="https://crontab.guru/" class="text-gray-300 hover:text-white font-bold">article</a>.
+                </p>
+            </div>
+            <div class="py-2">
+                <h3 class="text-white text-lg font-bold">What is Whoops?</h3>
+                <p class="text-sm text-gray-300">
+                    Whoops is a monitoring service for your website. Whoops monitors uptime, ssl certificates,
+                    broken links, performance, application health, and more. We send notifications everytime something goes wrong.
+                </p>
+            </div>
+        </div>
+        <div class="flex flex-col justify-start p-3 gap-3">
+            <div class="py-2">
+                <h3 class="text-white text-lg font-bold">What is a scheduler?</h3>
+                <p class="text-sm text-gray-300">
+                    A monitor is a collection of rules and alert settings that you will
+                    create for every cron job you want us to take care of. 
+                </p>
+            </div>
+            <div class="py-2">
+                <h3 class="text-white text-lg font-bold">Whoops sent me an alert when I didn't expect it?</h3>
+                <p class="text-sm text-gray-300">
+                    If you receive an alert you didn't expect the first thing to do is view your recent ping history
+                    in your dashboard. Most of the times the explanation can be found in the history, but please email
+                    us if you have any questions.
+                </p>
+            </div>
+            <div class="py-2">
+                <h3 class="text-white text-lg font-bold">How does Whoops compare to your competitors?</h3>
+                <p class="text-sm text-gray-300">
+                    We think that all our features combines all the monitoring services you need for your
+                    website. This way you can manage all your monitoring services from one place.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
